e7159ebf9f0f4c0f640f6171d4a5d552a7643b24
amimasoo <amirmasoud.dashti2762@gmail.com>
2019-05-13 12:36:03 +0430

    first Commit

---
+++ Assets/Scripts/GolemMovment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemMovment : MonoBehaviour {
+
+	private Rigidbody myBody;
+	private float moveForce = 10f;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody>();
+	}
+	
+
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		myBody.velocity = new Vector3(-h * moveForce, 0f, 0f);
+	}
+}
